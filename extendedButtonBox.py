2761babccb4496e4a46c18072f9bf5eab0b5a7b2
Plamena Milusheva <plamena@macathena.seattle.lmnarchitects.com>
2017-08-01 17:47:17 -0700

    partially working extended buttonBox

--- extendedButtonBox.py
+++ extendedButtonBox.py
@@ -2,10 +2,11 @@ import rhinoscriptsyntax as rs
 from datetime import datetime as dt, timedelta as td
 import scriptcontext
 
-TRACK = scriptcontex.sticky
+TRACK = scriptcontext.sticky
 T = "tick"
+TC = "tickcounter"
 H = "hold"
-C = "holdcounter"
+HC = "holdcounter"
 
 button_speeds = 0
 button_angles = [1500]*98
@@ -13,41 +14,58 @@ a = button_speeds
 hold_interval_us = td(microseconds = 1500000)
 run_interval_us = td(microseconds = 5000)
 
-TRACK[T] = dt.now()
+TRACK[T] = dt.now() + run_interval_us
 
 def triggered():
     triggered_buttons = []
-	for index, button in enumerate(buttons):		
-		if button == 1:
-            if index in triggered_buttons:
-                pass
-            else:
-                triggered_buttons.append(index)
-			TRACK[H] = dt.now() + hold_interval_us
-            TRACK[C] += 1
+    for index, button in enumerate(buttons):
+        if (button == 1) and (index not in triggered_buttons):
+            triggered_buttons.append(index)
+            TRACK[H] = dt.now() + hold_interval_us
+            TRACK[HC] += 1
         else:
-            TRACK[C] = 0
+            TRACK[H] = dt.now()
+            TRACK[HC] = 0
+    print triggered_buttons
+    print TRACK[H], TRACK[HC]
     return triggered_buttons
 
 def hold_action(current_time):
-    if (current_time >= TRACK[H]) and (TRACK[C] > 0):
+    t_buttons = triggered()
+    live_buttons = []
+    if (current_time >= TRACK[H]) and (TRACK[HC] > 0):
+        for count in range(TRACK[HC]):
+            for button in t_buttons:
+                live_buttons.append(proximity.Branch(*button))
+            t_buttons.append(*live_buttons)
+            print t_buttons
+    return t_buttons
+
+
+def ticker(current_time):
+   while 1:
+       if (current_time >= TRACK[T]) and (TRACK[TC] < 1000):
+           flipper(TRACK[TC], current_time)
+           TRACK[TC] += 1
+       elif (current_time >= TRACK[T]) and (TRACK[TC] >= 1000):
+           flipper(TRACK[TC], current_time)
+           TRACK[TC] -= 1000
+       else:
+           pass
+
+def flipper(remainder, current_time):
+   flip = hold_action(current_time)
+   for index, button in enumerate(flip):
+       if not flip:
+           if remainder % 2 == 0:
+               button_angles[index] = 1425
+           else:
+               button_angles[index] = 1525
 
 
 
+ticker(dt.now())       
 
-
-for index, button in enumerate(buttons):
-	hold_time_s = time.time()
-    if button == 1:
-    	hold_time_s += hold_interval_s
-        print index
-        if flip == 1:
-            button_angles[index] = 1425
-        else:
-            button_angles[index] = 1525
-    elif button == 1 and time.time() >= hold_time_s:
-
-            
+hold_action(dt.now())    
 print button_angles
 b = button_angles
- 
