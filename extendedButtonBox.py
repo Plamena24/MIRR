933d192b374a4f0e2eb3404047322f94666fc6d5
Plamena Milusheva <plamena@macathena.seattle.lmnarchitects.com>
2017-08-02 18:34:37 -0700

    partially working extendedButtonBox code and GH static testfile setup

--- extendedButtonBox.py
+++ extendedButtonBox.py
@@ -7,6 +7,7 @@ T = "tick"
 TC = "tickcounter"
 H = "hold"
 HC = "holdcounter"
+TR = "triggered_buttons"
 
 button_speeds = 0
 button_angles = [1500]*98
@@ -17,54 +18,54 @@ run_interval_us = td(microseconds = 5000)
 TRACK[T] = dt.now() + run_interval_us
 
 def triggered():
-    triggered_buttons = []
     for index, button in enumerate(buttons):
-        if (button == 1) and (index not in triggered_buttons):
-            triggered_buttons.append(index)
+        if (button == 1):
+            TRACK[TR] = [index]
             TRACK[H] = dt.now() + hold_interval_us
             TRACK[HC] += 1
         else:
+            TRACK[TR] = []
             TRACK[H] = dt.now()
             TRACK[HC] = 0
-    print triggered_buttons
-    print TRACK[H], TRACK[HC]
-    return triggered_buttons
+    print TRACK[TR]
+    print TRACK[H]
+    print TRACK[HC]
 
 def hold_action(current_time):
-    t_buttons = triggered()
-    live_buttons = []
+    triggered()
+    live_buttons = list(TRACK[TR])
     if (current_time >= TRACK[H]) and (TRACK[HC] > 0):
         for count in range(TRACK[HC]):
-            for button in t_buttons:
+            for button in TRACK[TR]:
                 live_buttons.append(proximity.Branch(*button))
-            t_buttons.append(*live_buttons)
-            print t_buttons
-    return t_buttons
+                print proximity.Branch(button)
+            TRACK[TR].append(*live_buttons)
+            print TRACK[TR]
 
 
-def ticker(current_time):
-   while 1:
-       if (current_time >= TRACK[T]) and (TRACK[TC] < 1000):
-           flipper(TRACK[TC], current_time)
-           TRACK[TC] += 1
-       elif (current_time >= TRACK[T]) and (TRACK[TC] >= 1000):
-           flipper(TRACK[TC], current_time)
-           TRACK[TC] -= 1000
-       else:
-           pass
-
-def flipper(remainder, current_time):
-   flip = hold_action(current_time)
-   for index, button in enumerate(flip):
-       if not flip:
-           if remainder % 2 == 0:
-               button_angles[index] = 1425
-           else:
-               button_angles[index] = 1525
-
-
-
-ticker(dt.now())       
+#def ticker(current_time):
+#   while 1:
+#       if (current_time >= TRACK[T]) and (TRACK[TC] < 1000):
+#           flipper(TRACK[TC], current_time)
+#           TRACK[TC] += 1
+#       elif (current_time >= TRACK[T]) and (TRACK[TC] >= 1000):
+#           flipper(TRACK[TC], current_time)
+#           TRACK[TC] -= 1000
+#       else:
+#           pass
+#
+#def flipper(remainder, current_time):
+#   flip = hold_action(current_time)
+#   for index, button in enumerate(flip):
+#       if not flip:
+#           if remainder % 2 == 0:
+#               button_angles[index] = 1425
+#           else:
+#               button_angles[index] = 1525
+#
+#
+#
+#ticker(dt.now())       
 
 hold_action(dt.now())    
 print button_angles
